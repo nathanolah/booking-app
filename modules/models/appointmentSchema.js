a332b21ed9097ceeaaafd77078877ace26fccc6c
jordanclark5646 <jordanclark5646@gmail.com>
2021-02-01 11:21:05 -0500

    added appointment schema

---
+++ modules/models/appointmentSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const customerModel = require('../models/customerSchema'); //not sure if needed
+const barberModel = require('../models/barberSchema'); //not sure if needed
+
+const appointmentSchema = new Schema({   
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    custID: {
+        type: Schema.ObjectId, ref: "Customer",
+        required: true        
+    },
+    barberID: {
+        type: Schema.ObjectId, ref: "Barbers",
+        required: true,      
+    },
+    isActive: {
+        type: Boolean,
+        required: true,        
+    }
+    
+});
+
+const appointmentModel = mongoose.model('Appointment', customerSchema);
+module.exports = appointmentModel;
