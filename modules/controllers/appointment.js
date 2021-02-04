50bca2cbea68746b78b8586506d7efaa3dd6dfeb
jordanclark5646 <jordanclark5646@gmail.com>
2021-02-04 11:57:47 -0500

    added update and remove

--- modules/controllers/appointment.js
+++ modules/controllers/appointment.js
@@ -15,15 +15,15 @@ router.post('/', (req, res) => {
     //adding date to appointment 
     let dateStr = temp.date.split(" ");
     let tempDateStart = new Date(dateStr[0], dateStr[1], dateStr[2], dateStr[3], dateStr[4]);
-    //let tempDateEnd = new Date (tempDateStart.getTime() + 45*60000); 
+    
     let realDateStart = new Date( tempDateStart.getTime() - Math.abs(tempDateStart.getTimezoneOffset()*-60000)) //removing utc offset
     let realDateEnd = new Date(realDateStart.getTime() + 45*60000)
-    //console.log(dateStr[0], dateStr[1], dateStr[2], dateStr[3], dateStr[4])
-    //adding isActive
+    
+    
     let isActiveTemp = true;
-    if(temp.isActive == "false"){
-        isActiveTemp = false; 
-    }
+    //if(temp.isActive == "false"){
+    //    isActiveTemp = false; 
+    //}
     
     appointmentModel.countDocuments({ 
         $and:[{
@@ -54,31 +54,9 @@ router.post('/', (req, res) => {
                         res.json(`New appointment: ${ newAppointment._id } was added to collection`);
                     }})
                 }
-            })
-        
+            })        
 
-    /*overLappingTimes = count(realDateStart, realDateEnd);
-    console.log(overLappingTimes);
-
-    if (overLappingTimes > 0){
-        res.json('Double booked, pick a different time')
-    }else{     
-    //putting values into mongoDB
-    let newAppointment = new appointmentModel();
-    newAppointment.custID = custID;
-    newAppointment.startDate = realDateStart;
-    newAppointment.endDate = realDateEnd;
-    newAppointment.barberID = barberID;
-    newAppointment.isActive = isActiveTemp;
-    //TO DO - ensure a barber can't get double booked
-    newAppointment.save((err) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(`New appointment: ${ newAppointment._id } was added to collection`);
-        }});
-        
-    }*/
+  
 
 });
 
@@ -92,6 +70,56 @@ router.get('/:id', (req, res) => {
 router.get('/', (req, res) => {
     appointmentModel.find().populate('custID').populate('barberID').exec()
         .then(appointments => res.json(appointments))
+        .catch(err => res.json(err));
+});
+
+router.put('/:id', (req, res) => {
+    const temp = req.body;
+    //adding customer and barber IDs to appointment
+    let ncustID = new mongoose.Types.ObjectId(temp.custID);
+    let barbID = new mongoose.Types.ObjectId(temp.barberID);
+    //adding date to appointment 
+    let dateStr = temp.date.split(" ");
+    let tempDateStart = new Date(dateStr[0], dateStr[1], dateStr[2], dateStr[3], dateStr[4]);
+    
+    let realDateStart = new Date( tempDateStart.getTime() - Math.abs(tempDateStart.getTimezoneOffset()*-60000)) //removing utc offset
+    let realDateEnd = new Date(realDateStart.getTime() + 45*60000)
+    
+    
+
+    appointmentModel.countDocuments({ 
+        $and:[{
+            
+            barberID: barbID,
+            $or:
+                [
+                    {
+                            endDate : { "$gte": new Date(realDateStart)}, startDate:{"$lte": new Date(realDateEnd)}
+                    }
+                ]
+                
+        }]}
+        ).then(count => {
+                if(count > 0){
+                    res.json('Double booked, pick a different time');
+                }else{
+                    //let newAppointment = new appointmentModel();
+                    //newAppointment.custID = custID;
+                    //newAppointment.startDate = realDateStart;
+                    //newAppointment.endDate = realDateEnd;
+                    //newAppointment.barberID = barbID;
+                    //newAppointment.isActive = isActiveTemp;    
+                appointmentModel.updateOne({_id: req.params.id}, {$set: {startDate: realDateStart}, endDate:realDateEnd, custID: ncustID, barberID: barbID})
+                    .then(res.json(`Appointment ${ req.params.id } successfully updated`))
+                    .catch(err => res.json(err));
+                    }
+            })   
+
+});
+
+router.delete('/:id', (req, res) => {
+    appointmentModel.deleteOne({_id: req.params.id}).exec()
+        .then(res.json(`Appointment ${ req.params.id } successfully deleted`))
         .catch(err => res.json(err));
 });
 
