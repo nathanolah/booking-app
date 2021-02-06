ed2a7e872cf8de5901ee523c3a7a74422948641d
unknown <nolah@myseneca.ca>
2021-02-06 18:13:05 -0500

    added sessions, route authentication & completed account update route

--- modules/controllers/account.js
+++ modules/controllers/account.js
@@ -6,9 +6,32 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // Added to get around the deprecation warning: "Mongoose: promise (mongoose's default promise library) is deprecated"
 
 const bcrypt = require('bcryptjs');
+const auth = require('../middleware/auth');
 
 // Account Model
 const accountModel = require('../models/accountSchema');
+
+/* Account Profile Route */
+
+// Get user's account profile
+router.get('/profile/:id', auth().isLoggedIn, (req, res) => {
+    // Checks if the route id is the same as the current sessions account id
+    if (req.session.account._id != req.params.id) {
+        res.json(`invalid id: ${ req.params.id }`);
+    } else {
+         // Display account details
+         accountModel.findById(req.params.id)
+         .then(account => res.json(account))
+         .catch(err => res.json(err));
+    }
+
+});
+
+// Account Logout (End Session)
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.json(`You have logged out`);
+});
 
 /* Account API Routes */
 
@@ -24,12 +47,6 @@ router.get('/:id', (req, res) => {
     accountModel.findOne({_id: req.params.id}).exec()
         .then(accounts => res.json(accounts))
         .catch(err => res.json(err));
-});
-
-// Account Logout (End Session)
-router.get('/logout', (req, res) => {
-    // DESTORY SESSION
-    // REDIRECT
 });
 
 // Account Login
@@ -64,8 +81,12 @@ router.post('/login', (req, res) => {
                         .then((isMatched) => {
                             if (isMatched == true) {
 
-                                //CREATE A SESSION FOR THIS ACCOUNT
-                                res.json(`Account: ${ account._id } has logged in`);
+                                // Create session for the user
+                                req.session.account = account;
+                                res.redirect(`/api/accounts/profile/${ account._id }`);
+
+                                //res.json(`Account: ${ account._id } has logged in`);
+                                //console.log(`Account: ${ account._id } has logged in`);
 
                             } else {
                                 res.json(`Sorry, your email and/or password is incorrect`);
@@ -122,7 +143,7 @@ router.post('/signup', (req, res) => {
                         firstName: firstName,
                         lastName: lastName,
                         email: email,
-                        password: password // TEST THIS
+                        password: password 
                     }
             
                     const newAccount = new accountModel(account);
@@ -131,6 +152,8 @@ router.post('/signup', (req, res) => {
                         if (err) {
                             res.json(err);
                         } else {
+                        
+                            // Redirect user to the login page
                             res.json(`New account: ${ newAccount._id } was added to collection`);
                         }
                     });
@@ -143,90 +166,208 @@ router.post('/signup', (req, res) => {
 
 });
 
-// Update account by id (NOT FINISHED)
-router.put('/update/:id', (req, res) => {
-    const errorMessage = [];
-
-    //const { firstName, lastName, email, password, cpassword } = req.body;
-    const { firstName, lastName } = req.body;
-
-    if (firstName == "" || lastName == "") {
-        errorMessage.push('You must enter a full name');
+// New password validations
+function checkPassword(password, cpassword, errorMessage) {
+    if (password.length < 6 || password.length > 12 && cpassword.length < 6 || cpassword.length > 12) {
+        errorMessage.push('Password must 6 to 12 characters long');
     }
+    if (!password.match(/^(?=.*\d)(?=.*[a-z]).{6,12}$/) && !cpassword.match(/^(?=.*\d)(?=.*[a-z]).{6,12}$/)) {
+        errorMessage.push('Password must contain letters and numbers');
+    }
+    if (password != cpassword) {
+        errorMessage.push('Passwords do not match');
+    }
+}
 
-    // if (email == "") {
-    //     errorMessage.push('You must enter an email');
-    // }
-
-    // // Password Validation
-    // if (password == "" || cpassword == "") {
-    //     errorMessage.push('You must enter a password');
-    // }
-    // if (password.length < 6 || password.length > 12 && cpassword.length < 6 || cpassword.length > 12) {
-    //     errorMessage.push('Password must 6 to 12 characters long');
-    // }
-    // if (!password.match(/^(?=.*\d)(?=.*[a-z]).{6,12}$/) && !cpassword.match(/^(?=.*\d)(?=.*[a-z]).{6,12}$/)) {
-    //     errorMessage.push('Password must contain letters and numbers');
-    // }
-    // if (password != cpassword) {
-    //     errorMessage.push('Passwords do not match');
-    // }
-
-
-    if (errorMessage.length > 0) {
-        res.json(errorMessage);
+// Update account by id 
+router.put('/update/:id', auth().isLoggedIn, (req, res) => {
+    if (req.session.account._id != req.params.id) {
+        res.json(`This account is not logged in`);
 
     } else {
-        accountModel.findOne({_id: req.body.id})
-            .then(account => {
+        accountModel.findOne({_id: req.params.id}).exec()
+            .then(acc => { 
+                let accDetails = {};
+                let passwordChanged = false;
 
-                const editAccount = {
-                    firstName: firstName,
-                    lastName: lastName
+                accDetails = acc;
+
+                // Update account details
+                const errorMessage = [];
+
+                const { firstName, lastName, email, password, cpassword } = req.body; //Details that are in the input fields
+
+                if (firstName == "" || lastName == "") {
+                    errorMessage.push('You must enter a full name');
                 }
 
-                const updatedAccount = new accountModel(editAccount);
+                if (email == "") {
+                    errorMessage.push('You must enter an email');
+                }
 
-                accountModel.updateOne({_id: req.params.id}, {$set: updatedAccount})
-                    .then(res.json(`Account ${ req.params.id } successfully updated`))
-                    .catch(err => res.json(err));
+                /* If the user has changed their current email */
+                if (accDetails.email != email) {
+
+                    // Check if new email already exists in the accounts collection
+                    accountModel.exists({email: email}, function(err, doc) {
+                        if (err) {
+                            res.json(err)
+                        } else {
+
+                            // Checks if new email already exists 
+                            if (doc == true) {
+                                errorMessage.push(`Email already exists`);
+                            }
+
+                            // Password Validation
+                            if (password == "" || cpassword == "") {
+                                errorMessage.push('You must enter a password');
+                            }
+
+                            // Password has been change from the original
+                            if (accDetails.password != password) { 
+                                passwordChanged = true;
+                                checkPassword(password, cpassword, errorMessage);
+                            }
+
+                            if (errorMessage.length > 0) {
+                                res.json(errorMessage);
+                            } else {
+                                // Update the account details
+                                
+                                // If the password has been changed
+                                if (passwordChanged == true) {
+                                    // Salt random generated character or strings
+                                    bcrypt.genSalt(12)
+                                    .then((salt) => {
+                                        let newPassword = password;
+                                        
+                                        bcrypt.hash(newPassword, salt)
+                                            .then((encryptPassword) => {
+                                                newPassword = encryptPassword;
+
+                                                // Update the account details
+                                                const editAccount = {
+                                                    firstName: firstName,
+                                                    lastName: lastName,
+                                                    email: email,
+                                                    password: newPassword
+                                                }
+                            
+                                                accountModel.updateOne({_id: req.params.id}, {$set: editAccount})
+                                                    .then(res.json(`Account ${ req.params.id } successfully updated`))
+                                                    .catch(err => res.json(err));
+
+
+                                                
+                                            })
+                                            .catch(err => console.log(`Error occured when hashing ${ err }`));
+                                    })
+                                    .catch(err => console.log(`Error occured when salting ${ err }`));
+            
+            
+                                } else {
+
+                                    // Update the account details
+                                    const editAccount = {
+                                        firstName: firstName,
+                                        lastName: lastName,
+                                        email: email,
+                                        password: password
+                                    }
+                
+                                    accountModel.updateOne({_id: req.params.id}, {$set: editAccount})
+                                        .then(res.json(`Account ${ req.params.id } successfully updated`))
+                                        .catch(err => res.json(err));
+
+                                }
+            
+                            }
+
+                        }
+
+                    });
+
+
+                } else {
+                    /** If the user hasn't changed their current email **/
+
+                    // Password Validation
+                    if (password == "" || cpassword == "") {
+                        errorMessage.push('You must enter a password');
+                    }
+
+                    // Password has been change from the original
+                    if (accDetails.password != password) { 
+                        passwordChanged = true;
+                        checkPassword(password, cpassword, errorMessage);
+                    }
+
+                    
+                    if (errorMessage.length > 0) {
+                        res.json(errorMessage);
+    
+                    } else {
+                        // Update the account details
+                        if (passwordChanged == true) {
+                            // Salt random generated character or strings
+                            bcrypt.genSalt(12)
+                            .then((salt) => {
+                                let newPassword = password;
+
+                                bcrypt.hash(newPassword, salt)
+                                    .then((encryptPassword) => {
+                                        newPassword = encryptPassword;
+
+                                        
+                                        // Update the account details
+                                        const editAccount = {
+                                            firstName: firstName,
+                                            lastName: lastName,
+                                            email: email,
+                                            password: newPassword
+                                        }
+                    
+                                        accountModel.updateOne({_id: req.params.id}, {$set: editAccount})
+                                            .then(res.json(`Account ${ req.params.id } successfully updated`))
+                                            .catch(err => res.json(err));
+
+
+                                    })
+                                    .catch(err => console.log(`Error occured when hashing ${ err }`));
+                            })
+                            .catch(err => console.log(`Error occured when salting ${ err }`));
+    
+    
+                        } else {
+                            // Update the account details
+                            const editAccount = {
+                                firstName: firstName,
+                                lastName: lastName,
+                                email: email,
+                                password: password
+                            }
+        
+                            accountModel.updateOne({_id: req.params.id}, {$set: editAccount})
+                                .then(res.json(`Account ${ req.params.id } successfully updated`))
+                                .catch(err => res.json(err));
+                            
+                        }
+    
+                    }
+
+                }
+
             })
             .catch(err => res.json(err));
 
-        // // Check for an existing email
-        // accountModel.findOne({email: req.body.email})
-        //     .then(email => {
-        //         if (email != null) {
-        //             res.json(`Email already exists`);
-                    
-        //         } else {
-
-        //             // Create new account object to update (this is to encrypt the new password)
-        //             const editAccount = {
-        //                 firstName: firstName,
-        //                 lastName: lastName,
-        //                 email: email, // EMAIL NOT CHANGING
-        //                 password: password // TEST THIS
-        //             }
-            
-        //             const updatedAccount = new accountModel(editAccount);
-            
-        //             accountModel.updateOne({_id: req.params.id}, {$set: updatedAccount})
-        //                 .then(res.json(`Account ${ req.params.id } successfully updated`))
-        //                 .catch(err => res.json(err));
-
-
-                    
-        //         }
-        //     })
-        //     .catch(err => res.json(err));
-
     }
+
 
 });
 
-// Delete account by id
-router.delete('/:id', (req, res) => {
+// Deletes an account by id in the account collection (This account must be logged in and system admin to delete)
+router.delete('/:id', auth().isAdmin, (req, res) => {
     accountModel.deleteOne({_id: req.params.id}).exec()
         .then(res.json(`Account ${ req.params.id } successfully deleted`))
         .catch(err => res.json(err));
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+
+// Stores the session in the database
+const MongoStore = require('connect-mongo')(session);
 
 // Load the environment file
 require('dotenv').config({ path: "./config/keys.env" });
@@ -14,6 +18,15 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(session({
+    secret: `${process.env.SESSION_KEY}`,
+    resave: false,
+    saveUninitialized: true, 
+    // cookie: { secure: true },
+
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 80 * 1000 }
+}));
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -26,7 +39,7 @@ app.use('/api/barbers', barberController);
 app.use('/api/accounts', accountController);
 app.use('/api/barberShops', barberShopController);
 
-// Promise operation asynchronous // REPLACE myData with process.env.MONGO_DB_URL
+// Promise operation asynchronous 
 mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log(`Connected to mongoDB`); // If promise is fulfilled
