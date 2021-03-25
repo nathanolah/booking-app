84192fb8c983064dcfbb6b3e2d2ce131f422b0a2
Nathan Olah <54871878+nathanolah@users.noreply.github.com>
2021-03-25 13:31:38 -0400

    Update barberSchema.js

--- modules/models/barberSchema.js
+++ modules/models/barberSchema.js
@@ -2,26 +2,24 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 const barberSchema = new Schema({
-    
-   
+   firstName: { type: String },
+   lastName: { type: String },
+   email: { type: String, unique: true },
+   dateCreated: { type: Date, default: Date.now() },
+   isManager: { type: Boolean, default:false},
+   password:{
+       type: String,
+       required:true
+   },
+   phoneNumber: { type: String },
+   schedules: [{ type: Schema.ObjectId, ref: 'Schedules' }],
+   reviews:[{
+       type: Schema.ObjectId, ref: 'Reviews'
+   }]
 
-
-    
-     firstName: { type: String },
-     lastName: { type: String },
-     email: { type: String, unique: true },
-     dateCreated: { type: Date, default: Date.now() },
-    // isManager: { type: Boolean },
-     phoneNumber: { type: String },
-     schedules: [{
-        type: Schema.ObjectId, ref: "Schedules"
-     }],
-     reviews:[{
-        type: Schema.ObjectId, ref: "Reviews"
-            
-     }],
 
 });
 
 const barberModel = mongoose.model('Barbers', barberSchema);
 module.exports = barberModel;
+
