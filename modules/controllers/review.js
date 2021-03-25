cb6bceb84028c79387ecde75e23d7f9cc64fa852
Nathan Olah <54871878+nathanolah@users.noreply.github.com>
2021-03-25 16:31:17 -0400

    Update review.js

--- modules/controllers/review.js
+++ modules/controllers/review.js
@@ -2,6 +2,8 @@ const express=require('express');
 const router=express.Router();
 
 const mongoose = require('mongoose');
+const barberModel = require('../models/barberSchema');
+const customerModel = require('../models/customerSchema');
 
 mongoose.Promise=global.Promise;
 
@@ -10,49 +12,60 @@ const reviewModel=require('../models/reviewSchema');
 
 
 router.post('/:id', (req, res) => {
-    const { ratings,  comments } = req.body;
+    const { ratings,  comments, author,} = req.body;
 
     if (ratings== null) {//comments can be blank
         res.json('You must select the rating');
 
     } else {
-        let newReview = new reviewModel(req.body);
-        newReview.save((err) => {
-            if (err) {
-                res.json(err);
-            } else {
-                barberModel.updateOne({_id: req.params.id}, {$push: {reviews: newReview}})
-            .then(res.json(`Barber ${ req.params.id } got new comment`))
-            .catch(err => res.json(err))}
-                //res.json(`New review: ${ newReview._id } was added to collection`);
+        customerModel.findById(author).then(customer=>{
+            if(customer==null)
+            {
+                res.json('Error')
             }
-        )
-            
-        
-       
-    }
-    
-});
+            else{
 
+                let newReview = new reviewModel(req.body);
+
+                newReview.save((err) => {
+
+                    if (err) {
+                    res.json(err);
+                    } 
+                    else 
+                    {
+                        barberModel.updateOne({_id: req.params.id}, {$push: {reviews: newReview}})
+                        .then(res.json(`Barber ${ req.params.id } got new comment`))
+                        .catch(err => res.json(err))}
+                    
+        
+                });
+            }
+
+        }).catch(err=>res.json(err));
+        
+    }
+
+});
 
 // Get all review 
 //not sure about this route since review is connected to each barber.
 router.get('/', (req, res) => {
-    reviewModel.find().exec()
+    reviewModel.find().populate('author').exec()
         .then(reviews => res.json(reviews))
         .catch(err => res.json(err));
 });
 
 // Get review by id
 router.get('/:id', (req, res) => {
-    reviewModel.findOne({_id: req.params.id}).exec()
+    reviewModel.findOne({_id: req.params.id}).populate('author').exec()
         .then(review => res.json(review))
         .catch(err => res.json(err));
 });
 
-// Update review by id
+// Update schedule by id
 router.put('/:id', (req, res) => {
-    const { ratings, comments } = req.body;
+    const { ratings, comments, valid } = req.body;
 
     if (ratings == null) {
         res.json('You must select the rating');
@@ -65,7 +78,7 @@ router.put('/:id', (req, res) => {
 
 });
 
-// Delete review by id
+// Delete schedule by id
 router.delete('/:id', (req, res) => {
     reviewModel.deleteOne({_id: req.params.id}).exec()
         .then(res.json(`Review ${ req.params.id } successfully deleted`))
@@ -73,4 +86,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
-
